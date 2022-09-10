0a2aad96774cac3ce48c68ea3f5a7e8f8d0986d2
mayureshw <mayureshw@github>
2022-09-10 15:56:22 +0530

    ifdef needed to skip cpp2xsb when not selected

--- pninfo.h
+++ pninfo.h
@@ -1,7 +1,10 @@
 #ifndef _PNINFO_H
 #define _PNINFO_H
 #include "petrinet.h"
+
+#ifdef PN_USE_EVENT_LISTENER
 #include "cpp2xsb.h"
+#endif
 
 // Structure to accumulate information as various classes build the Petri net
 
@@ -9,7 +12,9 @@ class PNInfo
 {
 public:
     Elements pnes;
+#ifdef PN_USE_EVENT_LISTENER
     Rel<unsigned,string,unsigned> vctid = {"vctid"};
+#endif
 };
 
 #endif
