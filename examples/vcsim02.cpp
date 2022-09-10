2968c952f27aa6aa278d20cc5bf16cbf1fe617df
mayureshw <mayureshw@github>
2022-09-10 20:06:22 +0530

    minor simplification to example

--- examples/vcsim02.cpp
+++ examples/vcsim02.cpp
@@ -7,7 +7,6 @@ int main()
 {
     Datum<int> a(8), b(8), c(8);
     a = 10; b = 20; c = 30;
-    vector<DatumBase*> inpv;
     vector<pair<string,vector<DatumBase*>>> feeds = 
         {
         {"p1",{&a,&b,&c}}
@@ -17,6 +16,6 @@ int main()
         {"p2",3}
         };
     map<string,vector<DatumBase*>> collectopmap;
-    vcsim("vcsim02.vc", "syspipe", inpv, feeds, collects, {}, collectopmap);
+    vcsim("vcsim02.vc", "syspipe", {}, feeds, collects, {}, collectopmap);
     return 0;
 }
