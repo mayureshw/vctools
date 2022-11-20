3facf5c7a0d2dd441faa25ce2568900f679c4a72
mayureshw <mayureshw@github>
2022-11-20 21:51:26 +0530

    collapsed distribution creator functions using macros

--- distributions.h
+++ distributions.h
@@ -38,80 +38,49 @@ public:
 // For constructor arguments, click on linke "(constructor)" on the distribution's page
 
 #define CREATOR(DISTNAME,ARITY) { #DISTNAME, { ARITY, &DistFactory::create_##DISTNAME } }
+#define CREATE1(DISTNAME,P1) \
+    static IDistribution* create_##DISTNAME(PTerm* t) \
+    { \
+        double P1 = getarg<float>(t, 0); \
+        return new Distribution< DISTNAME##_distribution<double>, double>(P1); \
+    }
+#define CREATE2(DISTNAME,P1,P2) \
+    static IDistribution* create_##DISTNAME(PTerm* t) \
+    { \
+        double P1 = getarg<float>(t, 0), P2 = getarg<float>(t, 1); \
+        return new Distribution< DISTNAME##_distribution<double>, double, double>(P1,P2); \
+    }
+
 class DistFactory
 {
     template<typename T> static T getarg(PTerm *t, int pos)
     {
         return PDb::atom2val<T>(t->args()[pos]);
     }
-    static IDistribution* create_uniform(PTerm* t)
-    {
-        double a = getarg<float>(t, 0), b = getarg<float>(t, 1);
-        return new Distribution< uniform_real_distribution<double>, double, double>(a,b);
-    }
-    static IDistribution* create_exponential(PTerm* t)
-    {
-        double lambda = getarg<float>(t, 0);
-        return new Distribution< exponential_distribution<double>, double>(lambda);
-    }
-    static IDistribution* create_gamma(PTerm* t)
-    {
-        double alpha = getarg<float>(t, 0), beta = getarg<float>(t, 1);
-        return new Distribution< gamma_distribution<double>, double, double>(alpha,beta);
-    }
-    static IDistribution* create_weibull(PTerm* t)
-    {
-        double a = getarg<float>(t, 0), b = getarg<float>(t, 1);
-        return new Distribution< weibull_distribution<double>, double, double>(a,b);
-    }
-    static IDistribution* create_extreme_value(PTerm* t)
-    {
-        double a = getarg<float>(t, 0), b = getarg<float>(t, 1);
-        return new Distribution< extreme_value_distribution<double>, double, double>(a,b);
-    }
-    static IDistribution* create_normal(PTerm* t)
-    {
-        double mean = getarg<float>(t, 0), stddev = getarg<float>(t, 1);
-        return new Distribution< normal_distribution<double>, double, double>(mean,stddev);
-    }
-    static IDistribution* create_lognormal(PTerm* t)
-    {
-        double m = getarg<float>(t, 0), s = getarg<float>(t, 1);
-        return new Distribution< lognormal_distribution<double>, double, double>(m,s);
-    }
-    static IDistribution* create_chi_squared(PTerm* t)
-    {
-        double n = getarg<float>(t, 0);
-        return new Distribution< chi_squared_distribution<double>, double>(n);
-    }
-    static IDistribution* create_cauchy(PTerm* t)
-    {
-        double a = getarg<float>(t, 0), b = getarg<float>(t, 1);
-        return new Distribution< cauchy_distribution<double>, double, double>(a,b);
-    }
-    static IDistribution* create_fisher_f(PTerm* t)
-    {
-        double m = getarg<float>(t, 0), n = getarg<float>(t, 1);
-        return new Distribution< fisher_f_distribution<double>, double, double>(m,n);
-    }
-    static IDistribution* create_student_t(PTerm* t)
-    {
-        double n = getarg<float>(t, 0);
-        return new Distribution< student_t_distribution<double>, double>(n);
-    }
+    CREATE2 ( uniform_real,  a,      b      )
+    CREATE1 ( exponential,   lambda         )
+    CREATE2 ( gamma,         alpha,  beta   )
+    CREATE2 ( weibull,       a,      b      )
+    CREATE2 ( extreme_value, a,      b      )
+    CREATE2 ( normal,        mean,   stddev )
+    CREATE2 ( lognormal,     m,      s      )
+    CREATE1 ( chi_squared,   n              )
+    CREATE2 ( cauchy,        a,      b      )
+    CREATE2 ( fisher_f,      m,      n      )
+    CREATE1 ( student_t,     n              )
     map< string, pair<int, function<IDistribution*(PTerm*)> >> _distcreators =
     {
-        CREATOR(uniform,      2),
-        CREATOR(exponential,  1),
-        CREATOR(gamma,        2),
-        CREATOR(weibull,      2),
-        CREATOR(extreme_value,2),
-        CREATOR(normal,       2),
-        CREATOR(lognormal,    2),
-        CREATOR(chi_squared,  1),
-        CREATOR(cauchy,       2),
-        CREATOR(fisher_f,     2),
-        CREATOR(student_t,    1),
+        CREATOR ( uniform_real,  2 ),
+        CREATOR ( exponential,   1 ),
+        CREATOR ( gamma,         2 ),
+        CREATOR ( weibull,       2 ),
+        CREATOR ( extreme_value, 2 ),
+        CREATOR ( normal,        2 ),
+        CREATOR ( lognormal,     2 ),
+        CREATOR ( chi_squared,   1 ),
+        CREATOR ( cauchy,        2 ),
+        CREATOR ( fisher_f,      2 ),
+        CREATOR ( student_t,     1 ),
     };
 public:
     IDistribution* get(PTerm* t)
