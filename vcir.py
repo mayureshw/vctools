baf2d391f7f3847404b5e3097b1102d343b839c2
mayureshw <mayureshw@github>
2023-05-24 11:10:04 +0530

    validations restored after coloring arcs

--- vcir.py
+++ vcir.py
@@ -8,11 +8,15 @@ class Node:
     def fanout(self,rel): return len(self.oarcs[rel])
     def successors(self,rel): return [ a.tgtnode for a in self.oarcs[rel] ]
     def predecessors(self,rel): return [ a.srcnode for a in self.iarcs[rel] ]
-    def _addOarc(self,rel,arc): self.oarcs[rel] += [arc]
-    def _addIarc(self,rel,arc): self.iarcs[rel] += [arc]
+    def _addOarc(self,rel,arc):
+        self.oarcs[rel] += [arc]
+        self.oarcs['all'] += [arc]
+    def _addIarc(self,rel,arc):
+        self.iarcs[rel] += [arc]
+        self.iarcs['all'] += [arc]
     def __init__(self,nodeid,vcir,props):
         self.vcir = vcir
-        arcrels = [ 'petri', 'mutex', 'passivebranch', 'branch' ]
+        arcrels = [ 'petri', 'mutex', 'passivebranch', 'branch', 'all' ]
         self.oarcs = { r:[] for r in arcrels }
         self.iarcs = { r:[] for r in arcrels }
         self.nodeid = nodeid
@@ -76,56 +80,56 @@ class VcPetriNet:
             tgtnode.addIarc(arcobj)
 
 class Vcir:
-    #def mutexFanInOuts(self):
-    #    mutexplaces = [ self.pn.nodes[mutex] for mutex in self.mutexes ]
-    #    for p in mutexplaces:
-    #        if p.fanin() != p.fanout():
-    #            print('fanin',p.fanin(),'fanout',p.fanout(),'mismatch for mutex place',p.nodeid,p.label)
-    #def branchPlaceType(self):
-    #    resolved_branches = self.mutexes.union(self.passive_branches).union(self.branches)
-    #    branchplaces = [ p for p in self.pn.places.values() if p.fanout() > 1 ]
-    #    for p in branchplaces:
-    #        if p.nodeid not in resolved_branches:
-    #            print('unresolved branch place:',p.nodeid,p.label)
-    #def atMostOneMutex(self):
-    #    mutexControlled = { s for m in self.mutexes for s in self.pn.nodes[m].successors() }
-    #    for mc in mutexControlled:
-    #        controllingMutexCnt = len(list(p for p in mc.predecessors() if p.nodeid in self.mutexes))
-    #        if controllingMutexCnt > 1 :
-    #            print('Transition may be controlled by at most 1 mutexes',mc.nodeid,mc.label,controllingMutexCnt)
-    #def highCapacityMustBePassive(self):
-    #    highCapPlaces = [ p for p in self.pn.places.values() if p.isHighCapacity() ]
-    #    for p in highCapPlaces:
-    #        if p.nodeid not in self.passive_branches:
-    #            print('Places with capacity > 1 must be passive branches',p.nodeid,p.label)
-    #def joinCapacity(self):
-    #    highJoinTrns = [ t for t in self.pn.transitions.values() if t.fanin > 4 ]
-    #    for t in highJoinTrns:
-    #        print('Joins up to fanin 4 supported (as of now)',t.nodeid,t.label,t.fanin())
-    #def branchFanout(self):
-    #    branchplaces = [ self.pn.places[p] for p in self.branches ]
-    #    for p in branchplaces:
-    #        if p.fanout() != 2 :
-    #            print('Branch fanout must be 2',p.nodeid,p.label,p.fanout())
-    #def highCapacityNotSupported(self):
-    #    highCapPlaces = [ p for p in self.pn.places.values() if p.isHighCapacity() ]
-    #    for p in highCapPlaces:
-    #        print('High capacity places not supported in asyncvhdl as of now',p.nodeid,p.label,p.capacity)
-    #def arcWtNotSupported(self):
-    #    highWtArcs = [ a for a in self.pn.arcs if a.wt > 1 ]
-    #    for a in highWtArcs:
-    #        print('High arc wt not supported in asyncvhdl as of now',a.srcnode.nodeid,'->',a.tgtnode.nodeid,a.wt)
-    #def checksNotAutomated(self):
-    #    print('Do run simulator with PN_PLACE_CAPACITY_EXCEPTION enabled. It is not checked by this tool.')
-    #    print('Do ensure, successors of passive branch are mutually exclusive. It is not checked, as it requires analysis such as unfoldings.')
-    #def validate(self):
-    #    self.mutexFanInOuts()
-    #    self.branchPlaceType()
-    #    self.atMostOneMutex()
-    #    self.highCapacityMustBePassive()
-    #    self.branchFanout()
-    #    self.highCapacityNotSupported()
-    #    self.arcWtNotSupported()
+    def mutexFanInOuts(self):
+        mutexplaces = [ self.pn.nodes[mutex] for mutex in self.mutexes ]
+        for p in mutexplaces:
+            if p.fanin('all') != p.fanout('all'):
+                print('fanin',p.fanin('all'),'fanout',p.fanout('all'),'mismatch for mutex place',p.nodeid,p.label)
+    def branchPlaceType(self):
+        resolved_branches = self.mutexes.union(self.passive_branches).union(self.branches)
+        branchplaces = [ p for p in self.pn.places.values() if p.fanout('all') > 1 ]
+        for p in branchplaces:
+            if p.nodeid not in resolved_branches:
+                print('unresolved branch place:',p.nodeid,p.label)
+    def atMostOneMutex(self):
+        mutexControlled = { s for m in self.mutexes for s in self.pn.nodes[m].successors('mutex') }
+        for mc in mutexControlled:
+            controllingMutexCnt = len(list(p for p in mc.predecessors('mutex')))
+            if controllingMutexCnt > 1 :
+                print('Transition may be controlled by at most 1 mutexes',mc.nodeid,mc.label,controllingMutexCnt)
+    def highCapacityMustBePassive(self):
+        highCapPlaces = [ p for p in self.pn.places.values() if p.isHighCapacity() ]
+        for p in highCapPlaces:
+            if not p.isPassiveBranch():
+                print('Places with capacity > 1 must be passive branches',p.nodeid,p.label)
+    def joinCapacity(self):
+        highJoinTrns = [ t for t in self.pn.transitions.values() if t.fanin('petri') > 4 ]
+        for t in highJoinTrns:
+            print('Joins up to fanin 4 supported (as of now)',t.nodeid,t.label,t.fanin('petri'))
+    def branchFanout(self):
+        branchplaces = [ self.pn.places[p] for p in self.branches ]
+        for p in branchplaces:
+            if p.fanout('branch') != 2 :
+                print('Branch fanout must be 2',p.nodeid,p.label,p.fanout('branch'))
+    def highCapacityNotSupported(self):
+        highCapPlaces = [ p for p in self.pn.places.values() if p.isHighCapacity() ]
+        for p in highCapPlaces:
+            print('High capacity places not supported in asyncvhdl as of now',p.nodeid,p.label,p.capacity)
+    def arcWtNotSupported(self):
+        highWtArcs = [ a for a in self.pn.arcs if a.wt > 1 ]
+        for a in highWtArcs:
+            print('High arc wt not supported in asyncvhdl as of now',a.srcnode.nodeid,'->',a.tgtnode.nodeid,a.wt)
+    def checksNotAutomated(self):
+        print('Do run simulator with PN_PLACE_CAPACITY_EXCEPTION enabled. It is not checked by this tool.')
+        print('Do ensure, successors of passive branch are mutually exclusive. It is not checked, as it requires analysis such as unfoldings.')
+    def validate(self):
+        self.mutexFanInOuts()
+        self.branchPlaceType()
+        self.atMostOneMutex()
+        self.highCapacityMustBePassive()
+        self.branchFanout()
+        self.highCapacityNotSupported()
+        self.arcWtNotSupported()
     def checkFilExists(self,flnm):
         if not os.path.exists(flnm):
             print('Did not find file', flnm)
