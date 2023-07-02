dddc32a8946c7a0a8034c54c466491280768e8f8
mayureshw <mayureshw@github>
2023-07-02 10:51:08 +0530

    show option added to dump properties

--- pnvalidations.py
+++ pnvalidations.py
@@ -2,10 +2,14 @@
 
 import sys
 from vcir import Vcir
+from vcirbase import NodeClass
 
 if len(sys.argv) != 2 :
     print('Usage:',sys.argv[0],'<vc-stem>')
     sys.exit(1)
 
 stem = sys.argv[1]
-vcir = Vcir(stem)
+if stem == 'show' :
+    NodeClass.printProps()
+else:
+    vcir = Vcir(stem)
