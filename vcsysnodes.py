8f9bf1b902ae228d641d6109f878bee8f171d8b7
mayureshw <mayureshw@github>
2024-06-10 16:03:52 +0530

    port class hierarchy added

--- vcsysnodes.py
+++ vcsysnodes.py
@@ -8,64 +8,58 @@ from vcdpnodes import *
 # This module does not need custom arc type. It reuses PN and DP arcs
 
 class SysNode(Node):
+    cntr = 0
     def dotprops(self): return [ ('color','gray') ]
     def optype(self): return None
     def nodeClass(self): return 'SysNode'
     def isSys(self): return True
     def idstr(self): return 'sys_' + str(self.nodeid)
-    def __init__(self,nodeid,vcir,props): super().__init__(nodeid,vcir,props)
+    def __init__(self,vcir,props):
+        super().__init__(vcir,props)
+        self.nodeid = SysNode.cntr
+        vcir.sysdp.nodes[self.nodeid] = self
+        SysNode.cntr = SysNode.cntr + 1
 
-class SysIfNode(SysNode):
-    def addParams(self,modulename,paramnames,widths):
-        self.module_params[modulename] = list(zip(paramnames,widths))
-    def __init__(self,nodeid,vcir,props): super().__init__(nodeid,vcir,props)
+class SysPortNode(SysNode):
+    def isPort(self): return True
+    def isControlPort(self): return False
 
-class SysEntryNode(SysIfNode):
-    def ithEntry(self,i): return self.oarcs['petri'][i].tgtnode
-    def createArcs(self):
-        for en in self.vcir.sysdp.nonCalledNonDaemonEns:
-            entrynode = self.vcir.pn.nodes[en]
-            width = sum(entrynode.owidths)
-            arcobj = PNArc({
-                'srcnode' : self,
-                'tgtnode' : entrynode,
-                'wt'      : 1,
-                })
-            self.addOarc(arcobj)
-            entrynode.addIarc(arcobj)
-            arcobj = DPArc({
-                'srcnode' : self,
-                'tgtnode' : entrynode,
-                'rel'     : 'bind',
-                'width'   : width
-                })
-            self.addOarc(arcobj)
-            entrynode.addIarc(arcobj)
-    def __init__(self,nodeid,vcir,props): super().__init__(nodeid,vcir,props)
+# Port directions
+class InPort:
+    direction = 'in'
+class OutPort:
+    direction = 'out'
 
-class SysExitNode(SysIfNode):
-    def ithExit(self,i): return self.iarcs['petri'][i].srcnode
-    def createArcs(self):
-        for en in self.vcir.sysdp.nonCalledNonDaemonEns:
-            ex = self.vcir.module_entries[en]['exit']
-            exitnode = self.vcir.pn.nodes[ex]
-            width = sum(exitnode.iwidths)
-            arcobj = PNArc({
-                'srcnode' : exitnode,
-                'tgtnode' : self,
-                'wt'      : 1,
-                })
-            self.addIarc(arcobj)
-            exitnode.addOarc(arcobj)
-            arcobj = DPArc({
-                'srcnode' : exitnode,
-                'tgtnode' : self,
-                'rel'     : 'bind',
-                'width'   : width
-                })
-            self.addIarc(arcobj)
-            exitnode.addOarc(arcobj)
-    def __init__(self,nodeid,vcir,props): super().__init__(nodeid,vcir,props)
+# Port sources
+class ModulePort:
+    def name(self): return self.modulename + self.suffix
+    def __init__(self,modulename): self.modulename = modulename
+class ModuleParamPort:
+    def name(self): return self.modulename + '_' + self.paramname
+    def __init__(self,modulename,paramname):
+        self.modulename = modulename
+        self.paramname = paramname
+class PipePort:
+    def name(self): return self.pipename + self.suffix
+    def __init__(self,pipename): self.pipename = pipename
+
+# Port roles
+class DataPort:
+    suffix = ''
+    def __init__(self,dwidth): self.dwidth = dwidth
+class ControlPort:
+    def isControlPort(self): return True
+class ReqPort(ControlPort):
+    suffix = '_req'
+class AckPort(ControlPort):
+    suffix = '_ack'
+
+def createPort(sysdp, ClsArgsList):
+    ClsList,ArgsList = list(zip(*ClsArgsList))
+    class Port(*ClsList,SysPortNode):
+        def __init__(self):
+            for cls,args in ClsArgsList: cls.__init__(self,*args)
+    sysdp.ports.append(Port())
 
 class SysPipeNode(SysNode):
     def optype(self): return 'Pipe'
@@ -73,159 +67,36 @@ class SysPipeNode(SysNode):
         ('color','gray'),
         ('label','pipe:'+self.name),
         ]
-    def createInternalReadArcs(self):
-        for dpe in self.vcir.dp.pipereads[self.name]:
-            # dpe <-> pipe bi-directional PN arcs
-            arcobj = PNArc({
-                'srcnode' : self,
-                'tgtnode' : dpe,
-                'wt'      : 1,
-                })
-            self.addIarc(arcobj)
-            dpe.addOarc(arcobj)
-
-            arcobj = PNArc({
-                'srcnode' : dpe,
-                'tgtnode' : self,
-                'wt'      : 1,
-                })
-            dpe.addIarc(arcobj)
-            self.addOarc(arcobj)
-
-            # pipe -> dpe data arc
-            arcobj = DPArc({
-                'srcnode' : self,
-                'tgtnode' : dpe,
-                'rel'     : 'data',
-                'width'   : self.width
-                })
-            dpe.addIarc(arcobj)
-            dpe.iwidths.append(self.width)
-            self.addOarc(arcobj)
-            self.owidths.append(self.width)
-    def createSysReadArcs(self):
-        sysExitNode = self.vcir.sysdp.sysExitNode
-        # pipe -> sysExit data bi-directional PN arcs
-        arcobj = PNArc({
-            'srcnode' : self,
-            'tgtnode' : sysExitNode,
-            'wt'      : 1,
-            })
-        self.addOarc(arcobj)
-        sysExitNode.addIarc(arcobj)
-
-        arcobj = PNArc({
-            'srcnode' : sysExitNode,
-            'tgtnode' : self,
-            'wt'      : 1,
-            })
-        sysExitNode.addOarc(arcobj)
-        self.addIarc(arcobj)
-
-        # pipe -> sysExit data
-        arcobj = DPArc({
-            'srcnode' : self,
-            'tgtnode' : sysExitNode,
-            'rel'     : 'data',
-            'width'   : self.width
-            })
-        self.addOarc(arcobj)
-        self.owidths.append(self.width)
-        sysExitNode.addIarc(arcobj)
-        sysExitNode.iwidths.append(self.width)
-    def createInternalFeedArcs(self):
-        for dpe in self.vcir.dp.pipefeeds[self.name]:
-            # dpe <-> pipe bi-directional PN arcs
-            arcobj = PNArc({
-                'srcnode' : self,
-                'tgtnode' : dpe,
-                'wt'      : 1,
-                })
-            self.addIarc(arcobj)
-            dpe.addOarc(arcobj)
-
-            arcobj = PNArc({
-                'srcnode' : dpe,
-                'tgtnode' : self,
-                'wt'      : 1,
-                })
-            dpe.addIarc(arcobj)
-            self.addOarc(arcobj)
-
-            # dpe -> pipe data arc
-            arcobj = DPArc({
-                'srcnode' : dpe,
-                'tgtnode' : self,
-                'rel'     : 'data',
-                'width'   : self.width
-                })
-            self.addIarc(arcobj)
-            self.iwidths.append(self.width)
-            dpe.addOarc(arcobj)
-            dpe.owidths.append(self.width)
-    def createSysFeedArcs(self):
-        sysEntryNode = self.vcir.sysdp.sysEntryNode
-        # sysEntry -> pipe data bi-directional PN arcs
-        arcobj = PNArc({
-            'srcnode' : sysEntryNode,
-            'tgtnode' : self,
-            'wt'      : 1,
-            })
-        sysEntryNode.addOarc(arcobj)
-        self.addIarc(arcobj)
-
-        arcobj = PNArc({
-            'srcnode' : self,
-            'tgtnode' : sysEntryNode,
-            'wt'      : 1,
-            })
-        self.addOarc(arcobj)
-        sysEntryNode.addIarc(arcobj)
-
-        # sysEntry -> pipe data
-        arcobj = DPArc({
-            'srcnode' : sysEntryNode,
-            'tgtnode' : self,
-            'rel'     : 'data',
-            'width'   : self.width
-            })
-        sysEntryNode.addOarc(arcobj)
-        sysEntryNode.owidths.append(self.width)
-        self.addIarc(arcobj)
-        self.iwidths.append(self.width)
-    def createArcs(self):
-        if self.name in self.vcir.dp.pipereads: self.createInternalReadArcs()
-        else: self.createSysReadArcs()
-        if self.name in self.vcir.dp.pipefeeds: self.createInternalFeedArcs()
-        else: self.createSysFeedArcs()
-    def __init__(self,nodeid,vcir,props): super().__init__(nodeid,vcir,props)
+    def __init__(self,vcir,props): super().__init__(vcir,props)
 
 class VCSysDP:
-    def createArcs(self):
-        for n in self.nodes.values(): n.createArcs()
+    def processModuleInterface(self,vcir,en):
+        moduledict = vcir.module_entries[en]
+        modulename = moduledict['name']
+        ex = moduledict['exit']
+        createPort(self, [ (InPort, []), (ModulePort,[modulename]), (ReqPort,[]) ])
+        createPort(self, [ (OutPort,[]), (ModulePort,[modulename]), (AckPort,[]) ])
+        for paramname,width in zip( moduledict['inames'], moduledict['iwidths'] ):
+            createPort(self, [
+                (InPort,[]),
+                (ModuleParamPort,[modulename,paramname]),
+                (DataPort,[width]),
+                ])
+        for paramname,width in zip( moduledict['onames'], moduledict['owidths'] ):
+            createPort(self, [
+                (OutPort,[]),
+                (ModuleParamPort,[modulename,paramname]),
+                (DataPort,[width]),
+                ])
     def __init__(self,vcir):
-        self.sysEntryNode = SysEntryNode(0,vcir,{
-            'module_params' : {}
-            })
-        self.sysExitNode = SysExitNode(1,vcir,{
-            'module_params' : {}
-            })
-        self.nodes = {
-            0 : self.sysEntryNode,
-            1 : self.sysExitNode,
-            }
-        nodeid = 2
+        self.nodes = {}
+        self.ports = []
         # They cease to meet the fanin criteria as sys nodes get added, so keep a copy
+        # TODO: Is this still needed?
         self.nonCalledNonDaemonEns = vcir.nonCalledNonDaemonEns()
-        # add param info to sysEntry and exit nodes
-        for en in self.nonCalledNonDaemonEns:
-            moduledict = vcir.module_entries[en]
-            self.sysEntryNode.addParams( moduledict['name'], moduledict['inames'], moduledict['iwidths'] )
-            self.sysExitNode.addParams( moduledict['name'], moduledict['onames'], moduledict['owidths'] )
-        # pipe nodes
-        for p,props in vcir.pipes.items():
-            n = SysPipeNode(nodeid,vcir,{**props,**{
-                'name' : p,
-                }})
-            self.nodes[nodeid] = n
-            nodeid = nodeid + 1
+        for en in self.nonCalledNonDaemonEns: self.processModuleInterface(vcir,en)
+        ## pipe nodes
+        #for p,props in vcir.pipes.items():
+        #    n = SysPipeNode(vcir,{**props,**{
+        #        'name' : p,
+        #        }})
