54aa4596f7ad229530f42f582a3d7266b32599c4
guest@nuc <guest@nuc>
2025-05-05 04:18:50 +0530

    uack2serq and sack2ureq made simu-only

--- vc2pn.h
+++ vc2pn.h
@@ -247,11 +247,13 @@ public:
         auto ureq_uack_place = pn()->createArc(_reqs[1], _acks[1]);
         pn()->annotatePNNode(ureq_uack_place, SimuOnly_);
 
-        pn()->createArc(_acks[0], _reqs[1]); // Since sreq/ureq can be ||, need this sync
+        auto sack2ureq = pn()->createArc(_acks[0], _reqs[1]); // Since sreq/ureq can be ||, need this sync
+        pn()->annotatePNNode( sack2ureq, SimuOnly_ );
         auto uack2sreq = (PNPlace*) pn()->createArc(
             _acks[1], _reqs[0],
             "MARKP:" + _reqs[0]->_name
             );
+        pn()->annotatePNNode( uack2sreq, SimuOnly_ );
         uack2sreq->setMarking(1);
         auto rootindex = elem()->Get_Root_Index();
 #       ifdef USECEP
